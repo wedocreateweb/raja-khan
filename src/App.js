fe5a1168cadfc2bf7a162919053c4bda4d1984cf
wedocreateweb <hiali68955@gmail.com>
2022-06-27 13:25:30 +0500

    updating app with a shop page, collection preview component and colleciton item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <Switch>
 
       <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
     </Switch>
   );
 }
