6e0b9e02e52ea6b06491f0c9d7cb50fdd0324542
wedocreateweb <hiali68955@gmail.com>
2022-09-15 21:02:44 +0500

    moved directory and shop data into its own reducer, created respective selectors and updated corresponding components with new ux flow for directong components with ux flow for directory/shop

---
+++ src/components/collections-overview/collections-overview.component.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { connect } from "react-redux";
+
+import { createStructuredSelector } from "reselect";
+
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import "./collections-overview.styles.scss";
+
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionsProps }) => (
+      <CollectionPreview key={id} {...otherCollectionsProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview,
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,22 @@
-import { combineReducers } from "redux";
-
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 import userReducer from "./user/user.reducer";
 import cartReducer from './cart/cart.reducer'
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
+import { combineReducers } from "redux";
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
