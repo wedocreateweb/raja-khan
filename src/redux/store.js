01c7510a7a4a738580857d1fa45a05cdf24be67d
wedocreateweb <hiali68955@gmail.com>
2022-10-03 02:57:57 +0500

    test pull

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
-
 import rootReducer from "./root-reducer";
 
 const middlewares = [];
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
+
+
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
